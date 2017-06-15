1d588a97b0e1b146a4d84cae7dfc9df1c088200e
Pranav Tripathi <pranav2579@gmail.com>
2017-06-15 22:03:50 +0530

    Wave Array

---
+++ Interviewbit/src/main/java/com/pranav/arrays/wave_array.java
@@ -0,0 +1,27 @@
+package com.pranav.arrays;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class wave_array {
+
+	public ArrayList<Integer> wave(ArrayList<Integer> a) {
+		Collections.sort(a);
+		int size = a.size();
+		int temp;
+		for (int i = 0; i < size - 1; i += 2) {
+			temp = a.get(i);
+			a.set(i, a.get(i + 1));
+			a.set(i + 1, temp);
+		}
+
+		return a;
+	}
+
+	public static void main(String[] args) {
+
+		// TODO Auto-generated method stub
+
+	}
+
+}
